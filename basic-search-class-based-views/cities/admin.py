40af4f98bf8f3d495b5e340fe8385130efe821ae
Chanrley Ferreira <35461240+chanrley@users.noreply.github.com>
2023-03-23 14:27:31 -0300

    Basic Search Class-Based-Views

---
+++ basic-search-class-based-views/cities/admin.py
@@ -0,0 +1,9 @@
+# cities/admin.py
+from django.contrib import admin
+
+from .models import City
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "state",)
+
+admin.site.register(City, CityAdmin)
